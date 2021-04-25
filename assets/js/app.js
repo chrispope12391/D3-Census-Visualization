2f2796efa84d9282a937289a0248443329f0b83d
chrispope12391 <popex107@umn.edu>
2021-04-24 19:54:52 -0500

    4

--- assets/js/app.js
+++ assets/js/app.js
@@ -34,3 +34,10 @@ var svg = d3.select("#scatter")
 
 var chartGroup = svg.append("g")
 .attr("transform", `translate(${margin.left}, ${margin.top})`);
+
+d3.csv("assets/data/data.csv").then(function(data) {
+
+    console.log(data);
+}).catch(function(error) {
+    console.log(error);
+});
