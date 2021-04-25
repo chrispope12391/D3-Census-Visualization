5225fb196074c6616534d016b14c323172cce79a
chrispope12391 <popex107@umn.edu>
2021-04-24 19:45:25 -0500

    3

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,1 +1,36 @@
 // @TODO: YOUR CODE HERE!
+console.log("app.js loaded");
+
+
+//if the svg area isn't empty when the browser loads,
+// remove it and replace it with a resized version of the chart
+var svgArea = d3.select("body").select("svg");
+
+// clear svg is not empty
+if (!svgArea.empty()) {
+    svgArea.remove();
+}
+
+// svg wrapper dimensions are determined by the current width and
+// height of the browser window.
+var svgWidth = window.innerWidth;
+var svgHeight = window.innerHeight;
+
+var margin = {
+    top: 50,
+    right: 50,
+    bottom: 50,
+    left: 50
+};
+
+var height = svgHeight - margin.top - margin.bottom;
+var width = svgWidth - margin.left - margin.right;
+
+
+var svg = d3.select("#scatter")
+.append("svg")
+.attr("height", svgHeight)
+.attr("width", svgWidth);
+
+var chartGroup = svg.append("g")
+.attr("transform", `translate(${margin.left}, ${margin.top})`);
